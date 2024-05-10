2e76bad180afe8b230d439e1914e5043c95443d7
mikestock-nimble <mike.stock@nimbleapproach.com>
2024-05-10 12:36:18 +0100

    changed IProjectTasksData to an interface, as it was mistakenly a class which caused binding problems

--- Dfe.Complete/Dfe.Complete.Data/Entities/IProjectTasksData.cs
+++ Dfe.Complete/Dfe.Complete.Data/Entities/IProjectTasksData.cs
@@ -7,18 +7,14 @@ namespace Dfe.Complete.Data.Entities
     /// Could do this as a class, but the problem is we have to modify a generated class to remove properties
     /// Likely this generated class will be changed multiple times as we try to stay in sync with the existing database
     /// </summary>
-    public class IProjectTasksData
+    public interface IProjectTasksData
     {
-        [NotMapped]
         public bool? HandoverReview { get; set; }
 
-        [NotMapped]
         public bool? HandoverNotes { get; set; }
 
-        [NotMapped]
         public bool? HandoverMeeting { get; set; }
 
-        [NotMapped]
         public bool? HandoverNotApplicable { get; set; }
     }
 }
